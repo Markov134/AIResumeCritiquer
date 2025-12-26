1b31d98ce6591baa240189ba0fefd667420c6487
Markov134 <stephanmarkovski@gmail.com>
2025-12-25 22:41:47 -0500

    Update main.py
    fixed title of website

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 st.set_page_config(page_title="AI Resume Critiquer", page_icon="📜", layout="centered")
 
-st.title("AI Resume Critiquer")
+st.title("📜 AI Resume Critiquer 📜")
 st.markdown("Upload your resume and get AI-powered feedback tailored to your needs!")
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
